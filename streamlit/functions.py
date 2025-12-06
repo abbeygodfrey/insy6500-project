8bc1bf9bedc43778414442e67ed2a04c3259d550
Abigail Godfrey <alg0072@auburn.edu>
2025-12-06 15:58:25 -0600

    finished streamlit

--- streamlit/functions.py
+++ streamlit/functions.py
@@ -1,37 +1,74 @@
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
 def load_data():
-	df_raw = pd.read_csv("~/insy6500/projects/insy6500_project/notebooks/Data.csv")
-	numeric_cols = df_raw.select_dtypes(include=[np.number]).columns.tolist()
-	def remove_outliers_iqr(dataframe, columns, threshold=1.5):
-    		df_clean = dataframe.copy()
-    		for col in columns: #first find interquartile range for each column
-        		Q1 = df_clean[col].quantile(0.25)
-        		Q3 = df_clean[col].quantile(0.75)
-        		IQR = Q3 - Q1
-        
-        		# create upper and lower limits
-        		lower_bound = Q1 - threshold * IQR
-        		upper_bound = Q3 + threshold * IQR
-        
-        		# git rid of outliers
-        		df_clean = df_clean[(df_clean[col] >= lower_bound) & (df_clean[col] <= upper_bound)]
-    
-    		return df_clean
+    df_raw = pd.read_csv("../notebooks/Data.csv")
+    numeric_cols = df_raw.select_dtypes(include=[np.number]).columns.tolist()
 
-	# remove outliers from data
-	numeric_cols.remove('high_risk_flag')
-	df_cleaned = remove_outliers_iqr(df_raw, numeric_cols, threshold=1.5)
-	return df_cleaned
+    def remove_outliers_iqr(dataframe, columns, threshold=1.5):
+        df_clean = dataframe.copy()
+        for col in columns:
+            Q1 = df_clean[col].quantile(0.25)
+            Q3 = df_clean[col].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - threshold * IQR
+            upper_bound = Q3 + threshold * IQR
+            df_clean = df_clean[
+                (df_clean[col] >= lower_bound) & (df_clean[col] <= upper_bound)
+            ]
+        return df_clean
+
+    numeric_cols.remove('high_risk_flag')
+    df_cleaned = remove_outliers_iqr(df_raw, numeric_cols, threshold=1.5)
+    return df_cleaned
 
 
 def create_corr(df_cleaned):
-	numeric_cols = df_cleaned.select_dtypes(include=[np.number]).columns.tolist()
-	numeric_cols.remove('high_risk_flag')
-	corr = df_cleaned[numeric_cols].corr()
-	sns.heatmap(corr)
-	fig = plt.show
-	return fig
+    numeric_cols = df_cleaned.select_dtypes(include=[np.number]).columns.tolist()
+    numeric_cols.remove('high_risk_flag')
+
+    fig, ax = plt.subplots()
+    corr = df_cleaned[numeric_cols].corr()
+    sns.heatmap(corr, ax=ax)
+    return fig
+
+def create_hist(df_cleaned):
+    df_cleaned = df_cleaned.copy()
+    df_cleaned['screentime_bucket'] = pd.cut(
+        df_cleaned['device_hours_per_day'],
+        bins=[0, 3, 6, 9, float("inf")],
+        labels=["0-3", "3-6", "6-9", "9+"],
+        right=False
+    )
+    fig, ax = plt.subplots()
+    sns.histplot(
+        data=df_cleaned,
+        x="depression_score",
+        hue="screentime_bucket",
+        multiple="stack",
+        ax=ax
+    )
+    ax.set_xlabel('Depression')
+    ax.set_title('Depression Density based on Screentime')
+    return fig
+
+def split_hist(df_cleaned):
+    df_cleaned = df_cleaned.copy()
+    df_cleaned['screentime_bucket'] = pd.cut(
+        df_cleaned['device_hours_per_day'],
+        bins=[0, 3, 6, 9, float("inf")],
+        labels=["0-3", "3-6", "6-9", "9+"],
+        right=False
+    )
+    # displot is figure-level and returns a FacetGrid
+    g = sns.displot(
+        data=df_cleaned,
+        x="depression_score",
+        hue="screentime_bucket",
+        col="screentime_bucket"
+    )
+    fig = g.fig  # get the underlying matplotlib Figure
+    return fig
--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from functions import load_data, create_corr
+from functions import load_data, create_hist, split_hist
 from PIL import Image
 
 st.set_page_config(page_title = 'EDA Project - Digital Lifestyle Benchmark', layout = 'wide')
@@ -7,13 +7,13 @@ st.title("Digital Lifestyle Benchmark Data Analysis")
 st.write('''
 ### Project Description
 
-This project strengthens skill sets pertaining to data exploration, cleaning, and analysis. The “real world” data set selected for this project is the “Digital 
+This project strengthens skill sets pertaining to data exploration, cleaning, and analysis. The “real world” data set selected for this project is “Digital 
 Lifestyle Benchmark,” a data set found on Kaggle that explores the correlations between mental/emotional wellbeing and social media interaction/studying 
 online/screen time.  To begin analysis of the data set, the data was cleaned, meaning null values and outliers were removed if present. Along with this, the 
 data columns were properly assigned data types to ensure efficient and easy analysis. Afterwards, hypotheses were drawn regarding the correlations between 
 values and tested via data visualization during analysis.''')
 
-#df = load_data()
+df = load_data()
 
 #fig = create_corr(df)
 #st.pyplot(fig)
@@ -84,7 +84,7 @@ st.write("""#### When comparing age groups with similar screen time, is there a 
 We assume that there is a strong correlation between mental illness and age, with 
 mental illness levels sharing a positive correlation with screen time. We 
 anticipated that teenagers and young adults will have higher levels of 
-depression, stress, and anxiety. 
+mental illness in comparison to those older in age. 
 """)
 
 a_a = Image.open('a_a.png')
@@ -106,12 +106,99 @@ st.write("""While there are no correlations between age versus depression, anxie
 and stress, the three graphs highlight the strong positive correlation between 
 higher screen times and mental illnesses.""")
 
+st.write("""#### Is there a correlation between time spent studying online and mental illness?""")
+
+st.write("""We speculate there is a strong positive correlation 
+between studying online and mental illness. We assume that more time 
+spent studying with a screen will lead to higher levels of mental 
+illness.""")
+
+s_a = Image.open('s_a.png')
+s_d = Image.open('s_d.png')
+s_s = Image.open('s_s.png')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+        st.image(s_a, caption = "Time Spent Studying Online vs Anxiety")
+
+with col2:
+        st.image(s_d, caption = "Time Spent Studying Online vs Depression")
+
+with col3:
+        st.image(s_s, caption = "Time Spent Studying Online vs Stress")
+
+st.write("""There is very little variation between the separate time 
+spent studying groups. This means that there is no correlation among 
+studying online and mental illness, which is contradictory to our 
+hypothesis.""")
+
+st.write("""#### What are the effects of social media usage and happiness scores? Is there a correlation?""")
+
+st.write("""We hypothesize there is a strong inverse relationship between happiness and social media usage. In 
+this relationship, we assume more time spent on social media will lead to lower happiness scores.""")
 
 h_a = Image.open('h_a.png')
 st.image(h_a, caption = "Happiness vs Anxiety")
 
+st.write("""The data indicates there is no variation with happiness 
+and depression scores as social media minutes increase, meaning a lack 
+of correlation.""")
+
+st.write("""#### Is there a correlation between focus scores and time spent on social media?""")
+
+st.write("""We predict an inverse relationship between focus scores 
+and social media usage. In this case, as social media usage 
+increases, the focus score will decrease.""")
+
 s_f = Image.open('s_f.png')
 st.image(s_f, caption = "Affect of Time Spent on Social Media on Focus")
 
+st.write("""As expected, there is an inverse relationship between 
+focus and social media usage; as usage increases focus scores 
+decrease.""")
+
+st.write("""#### Do notifications induce higher levels of stress?""")
+
+st.write("""It is hypothesized that a positive correlation lies 
+between number of notifications and levels of stress. We expect that 
+an increase in notifications will lead to higher levels of 
+stress.""")
+
 s_n = Image.open('s_n.png')
 st.image(s_n, caption = "Affect of Number of Notifications on Stress")
+
+st.write("""The data proves the hypothesis, there is a positive 
+trend between social media usage and stress levels.""")
+
+st.write("""#### What is the correlation between time spent on a device and sleep quality?""")
+
+st.write("""We predict there is an inverse relationship between 
+device usage and sleep quality. In this relationship, as device 
+hours per day increases, we expect sleep quality levels to 
+decrease.""")
+
+sq = Image.open('sq.png')
+st.image(sq, caption = "Affect of Device Hours per Day on Sleep Quality")
+
+st.write("""As expected, as device hours increase, sleep quality 
+decreases. Indicating there is a strong inverse relationship between 
+the two factors, thus proving the hypothesis.""")
+
+st.write("""#### Is there a correlation between depression and screen time?""")
+ 
+st.write("""We expect a strong correlation between depression and 
+screen time. It is hypothesized that an increase in screen time will 
+showcase a rise in depression scores.""")
+
+if st.button('Click for Stacked Graph'):
+	fig = create_hist(df)
+	st.pyplot(fig)
+
+if st.button('Click for Split Graph'):
+	fig = split_hist(df)
+	st.pyplot(fig)
+
+st.write("""This combined bar chart shows higher density levels of 
+depression as screen time increases. This indicates a strong 
+positive correlation between depression and screen time.""")
